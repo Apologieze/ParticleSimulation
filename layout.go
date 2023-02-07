dc17b276731e40f03721c3d89fcac79da46d6710
Apologieze <47333595+Apologieze@users.noreply.github.com>
2023-02-07 16:22:25 +0100

    FIx dpi bug

--- layout.go
+++ layout.go
@@ -1,7 +1,10 @@
 package main
 
+import "github.com/hajimehoshi/ebiten/v2"
+
 // Layout définit la taille en pixels de la zone d'affichage des particules
 // en fonction de la taille en pixels de la fenêtre.
 func (g *game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return outsideWidth, outsideHeight
+	s := ebiten.DeviceScaleFactor()
+	return int(float64(outsideWidth) * s), int(float64(outsideHeight) * s)
 }
